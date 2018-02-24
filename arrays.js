0c7aacd7dd3c30d3484169058b1851a0a1f01f7f
Aaron Gibson <aaron.gibson13@gmail.com>
2018-02-24 15:18:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,7 +27,7 @@ function destructivelyAddElementToEndOfArray (array, entry) {
 
 function accessElementInArray (array, index) {
   //acces an array with an index and return that value
-  return array[entry]
+  return array[index]
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
